bc086008d848ac1fb479ceaa280c41e8f3ff0be8
Veltays <yassine.gran@student.hepl.be>
2024-11-23 13:53:53 +0100

    C'est le bordel mais le serveur renvoie bien un message au client

--- Exercice5/Serveur.cpp
+++ Exercice5/Serveur.cpp
@@ -6,6 +6,7 @@
 #include <signal.h>
 #include "protocole.h" // contient la cle et la structure d'un message
 #include <fcntl.h>
+#include <cstring>
 
 int idQ;
 int pid1, pid2;
@@ -22,29 +23,24 @@ int main()
   fprintf(stderr, "(SERVEUR) Creation de la file de messages\n");
   // TO DO (etape 2)
 
-  if ((idQ = msgget(CLE, IPC_CREAT | 0666)) == -1) //Va créer une file de msg
-    fprintf(stderr, "(SERVEUR) Probleme lors de la creation de la fille \n");
-  else 
-    fprintf(stderr, "(SERVEUR) la fille à bien été créer");
+    if ((idQ = msgget(CLE, IPC_CREAT | 0666)) == -1) //Va créer une file de msg
+      fprintf(stderr, "(SERVEUR) Probleme lors de la creation de la fille \n");
+    else
+      fprintf(stderr, "(SERVEUR) la fille à bien été créer");
 
+    if ((msgrcv(idQ, &requete, sizeof(MESSAGE) - sizeof(long), 1, 0)) == -1)
+      fprintf(stderr, "(SERVEUR) Probleme lors la reception du fichier");
+    else
+    {
+      printf("Message reçu de (type %ld) : %s vers %d\n", requete.type, requete.texte, requete.expediteur);
+      destinataire = requete.expediteur;
 
-
-  if ((msgrcv(idQ, &requete, sizeof(MESSAGE), 1, 0)) == -1)
-    fprintf(stderr, "(SERVEUR) Probleme lors la reception du fichier");
-  else  
-     printf("Message reçu (type %ld) : %s\n", requete.type, requete.texte);
-
-
-
-
-
-
-
-
-
-
-
-
+      if ((msgsnd(idQ, &requete, sizeof(MESSAGE) - sizeof(long), 0)) == -1)
+        fprintf(stderr, "(SERVEUR) Le message n'a pas pu étre envoyer\n");
+      else
+        fprintf(stderr, "(SERVEUR) Le message a été envoyer avec succes\n");
+    
+  } 
 
 
 
@@ -55,6 +51,10 @@ int main()
   // TO DO (etape 5)
   fprintf(stderr, "(SERVEUR) Attente de connection d'un second client...\n");
   // TO DO (etape 5)
+
+
+
+
 
   //while(1)
   {
--- Exercice5/windowclient.cpp
+++ Exercice5/windowclient.cpp
@@ -16,28 +16,23 @@ extern WindowClient *w;
 
 extern char nomClient[40];
 int idQ; // identifiant de la file de message
-
+MESSAGE requeteC;
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-WindowClient::WindowClient(QWidget *parent):QMainWindow(parent),ui(new Ui::WindowClient)
+WindowClient::WindowClient(QWidget *parent) : QMainWindow(parent), ui(new Ui::WindowClient)
 {
-  MESSAGE requeteC;
   ui->setupUi(this);
   setWindowTitle(nomClient);
-  
+
   // Recuperation de l'identifiant de la file de messages
-  fprintf(stderr,"(CLIENT %s %d) Recuperation de l'id de la file de messages\n",nomClient,getpid());
+  fprintf(stderr, "(CLIENT %s %d) Recuperation de l'id de la file de messages\n", nomClient, getpid());
   // TO DO (etape 2)
-  
-  if((idQ = msgget(CLE,IPC_CREAT | 0666)) == -1)
-    fprintf(stderr,"(CLIENT) Probleme lors de la connexion de la fille");
+
+  if ((idQ = msgget(CLE, IPC_CREAT | 0666)) == -1)
+    fprintf(stderr, "(CLIENT) Probleme lors de la connexion de la fille\n");
   else
-    fprintf(stderr,"(CLIENT) la file à bien été trouver");
-
-
-  strcpy(requeteC.texte,"salut");
-  
+    fprintf(stderr, "(CLIENT) la file à bien été trouver \n");
 
   // Envoi d'une requete d'identification
   // TO DO (etape 5)
@@ -54,10 +49,10 @@ WindowClient::~WindowClient()
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///// Fonctions utiles : ne pas modifier /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void WindowClient::setRecu(const char* Text)
+void WindowClient::setRecu(const char *Text)
 {
   //fprintf(stderr,"---%s---\n",Text);
-  if (strlen(Text) == 0 )
+  if (strlen(Text) == 0)
   {
     ui->lineEditRecu->clear();
     return;
@@ -65,10 +60,10 @@ void WindowClient::setRecu(const char* Text)
   ui->lineEditRecu->setText(Text);
 }
 
-void WindowClient::setAEnvoyer(const char* Text)
+void WindowClient::setAEnvoyer(const char *Text)
 {
   //fprintf(stderr,"---%s---\n",Text);
-  if (strlen(Text) == 0 )
+  if (strlen(Text) == 0)
   {
     ui->lineEditEnvoyer->clear();
     return;
@@ -76,21 +71,21 @@ void WindowClient::setAEnvoyer(const char* Text)
   ui->lineEditEnvoyer->setText(Text);
 }
 
-const char* WindowClient::getAEnvoyer()
+const char *WindowClient::getAEnvoyer()
 {
   if (ui->lineEditEnvoyer->text().size())
-  { 
-    strcpy(aEnvoyer,ui->lineEditEnvoyer->text().toStdString().c_str());
+  {
+    strcpy(aEnvoyer, ui->lineEditEnvoyer->text().toStdString().c_str());
     return aEnvoyer;
   }
   return NULL;
 }
 
-const char* WindowClient::getRecu()
+const char *WindowClient::getRecu()
 {
   if (ui->lineEditRecu->text().size())
-  { 
-    strcpy(recu,ui->lineEditRecu->text().toStdString().c_str());
+  {
+    strcpy(recu, ui->lineEditRecu->text().toStdString().c_str());
     return recu;
   }
   return NULL;
@@ -101,13 +96,36 @@ const char* WindowClient::getRecu()
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void WindowClient::on_pushButtonEnvoyer_clicked()
 {
-  fprintf(stderr,"Clic sur le bouton Envoyer\n");
+  fprintf(stderr, "Clic sur le bouton Envoyer\n");
+  //Initialition du message
+  strcpy(requeteC.texte, getAEnvoyer()); // On copie ce qui est écrit dans le programme dans ma structure
+  requeteC.type = 1;
+  requeteC.expediteur = getpid();
+  fprintf(stderr, "(CLIENT) voici votre message %s \n", requeteC.texte);
+
+  if ((msgsnd(idQ, &requeteC, sizeof(MESSAGE) - sizeof(long), 0)) == -1)
+    fprintf(stderr, "(CLIENT) Le message n'a pas pu étre envoyer\n");
+  else
+    fprintf(stderr, "(CLIENT) Le message a été envoyer avec succes\n");
   // TO DO (etapes 2, 3, 4)
+  while (1)
+  {
+
+    if ((msgrcv(idQ, &requeteC, sizeof(MESSAGE) - sizeof(long), 1, 0)) == -1)
+      fprintf(stderr, "(CLIENT) Le message n'a pas pu étre recuperer\n");
+    else
+    {
+      fprintf(stderr, "(CLIENT) Le message a pas pu étre recuperer !!! \n");
+      printf("Message reçu de (type %ld) : %s vers %d\n", requeteC.type, requeteC.texte, requeteC.expediteur);
+      setRecu(requeteC.texte);
+      break;
+    }
+  }
 }
 
 void WindowClient::on_pushButtonQuitter_clicked()
 {
-  fprintf(stderr,"Clic sur le bouton Quitter\n");
+  fprintf(stderr, "Clic sur le bouton Quitter\n");
   exit(1);
 }
 
